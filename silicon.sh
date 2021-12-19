55ea59f6617a4c9eec896c497d15ac5d9717993a
tqfx <tqfx@foxmail.com>
2021-12-20 02:06:02 +0800

    fix(silicon.sh): unset font

--- silicon.sh
+++ silicon.sh
@@ -2,7 +2,6 @@
 FLAGS+=" --no-line-number"
 FLAGS+=" --no-round-corner"
 FLAGS+=" --no-window-controls"
-FLAGS+=" --font Consolas"
 FLAGS+=" --line-offset 0"
 FLAGS+=" --line-pad 0"
 FLAGS+=" --pad-horiz 0"
