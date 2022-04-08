79e75fb098ea896bf17c6b483426758587813d33
tqfx <tqfx@foxmail.com>
2022-04-08 10:35:05 +0800

    fix(git-config-tqfx.sh):

--- git-config-tqfx.sh
+++ git-config-tqfx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-git config $@ user.name       tqfx
-git config $@ user.email      tqfx@foxmail.com
-git config $@ user.signingkey 54B434B4E27F1DCB
-git config $@ --unset         core.sshCommand
-git config $@ --list
+git config "$@" user.name       tqfx
+git config "$@" user.email      tqfx@foxmail.com
+git config "$@" user.signingkey 54B434B4E27F1DCB
+git config "$@" --unset         core.sshCommand
+git config "$@" --list
