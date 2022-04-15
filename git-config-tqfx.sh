b62b0b463465ce8d9670b59eebdd4725d8f854c9
tqfx <tqfx@foxmail.com>
2022-04-15 12:58:40 +0800

    2022-04-15T12:58:40+08:00

--- git-config-tqfx.sh
+++ git-config-tqfx.sh
@@ -3,4 +3,3 @@ git config "$@" user.name       tqfx
 git config "$@" user.email      tqfx@foxmail.com
 git config "$@" user.signingkey 54B434B4E27F1DCB
 git config "$@" --unset         core.sshCommand
-git config "$@" --list
