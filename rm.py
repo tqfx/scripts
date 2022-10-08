8a358f2f82334da64b8705d943a806933b2e483f
tqfx <tqfx@foxmail.com>
2022-10-08 22:02:55 +0800

    refact: rm

--- rm.py
+++ rm.py
@@ -1,135 +1,115 @@
 #!/usr/bin/env python
+import argparse
 import hashlib
-import getopt
-import sys
 import os
 
 
-opt_verbose = False
-opt_remove = False
-opt_rename = False
-opt_empty = False
-opt_files = []
-opt_dirs = []
+def hash_(file: str, name: str = "MD5", BUFSIZ=0x2000) -> str:
+    hashes = hashlib.new(name)
+    with open(file, "rb") as f:
+        buffer = f.read(BUFSIZ)
+        while buffer:
+            hashes.update(buffer)
+            buffer = f.read(BUFSIZ)
+    return hashes.hexdigest()
 
 
-def usage() -> None:
-    help = "{} [option] [dirs]\n".format(sys.argv[0])
-    help += "-v --verbose\t- 详细显示进行的步骤\n"
-    help += "-r --remove\t- 删除文件\n"
-    help += "-R --rename\t- 重命名文件\n"
-    help += "-e --empty\t- 选择空文件\n"
-    help += "-h --help\t- 显示此帮助信息并退出"
-    print(help)
+class rm:
+    def __init__(self, remove=False, rename=False, verbose=False) -> None:
+        self.verbose = verbose
+        self.rename_ = rename
+        self.remove_ = remove
 
+    def __call__(self, top):
+        for dirpath, dirnames, filenames in os.walk(top):
+            del dirnames
+            if self.verbose:
+                print(dirpath)
+            self.files = []
+            for filename in filenames:
+                filename = os.path.join(dirpath, filename)
+                filesize = os.path.getsize(filename)
+                filehash = hash_(filename)
+                self.files.append((filename, filesize, filehash))
+            if self.files:
+                self.files.sort(key=lambda x: x[1])
+                self.init().rename().remove().exit()
+            del self.files
+        return self
 
-try:
-    opts, args = getopt.getopt(
-        sys.argv[1:], "hvrRe", ["help", "verbose", "remove", "rename", "empty"]
-    )
-except getopt.GetoptError:
-    exit(usage())
-for arg in args:
-    if os.path.isfile(arg):
-        opt_files.append(arg)
-    elif os.path.isdir(arg):
-        opt_dirs.append(arg)
-    else:
-        print("未知", arg)
-for opt, arg in opts:
-    if opt in ("-h", "--help"):
-        exit(usage())
-    elif opt in ("-v", "--verbose"):
-        opt_verbose = True
-    elif opt in ("-r", "--remove"):
-        opt_remove = True
-    elif opt in ("-R", "--rename"):
-        opt_rename = True
-    elif opt in ("-e", "--empty"):
-        opt_empty = True
-
-
-def exe_remove(filenames: list) -> None:
-    removes = set()
-    if not filenames:
-        return
-    for i, file1 in enumerate(filenames):
-        size1 = os.path.getsize(file1)
-        if opt_empty and size1 == 0:
-            removes.add(file1)
-            continue
-        m1 = ''
-        for j, file2 in enumerate(filenames):
-            if i == j:
-                continue
-            size2 = os.path.getsize(file2)
-            if size1 == size2:
-                if m1 == '':
-                    with open(file1, "rb") as f1:
-                        m1 = hashlib.md5(f1.read()).hexdigest()
-                with open(file2, "rb") as f2:
-                    m2 = hashlib.md5(f2.read()).hexdigest()
-                if m1 == m2:
-                    if os.path.getmtime(file1) > os.path.getmtime(file2):
-                        removes.add(file1)
-                    else:
-                        removes.add(file2)
-    for filename in removes:
-        if opt_remove:
-            os.remove(filename)
-        if opt_verbose:
-            print("删除", filename)
-
-
-def exe_rename(filenames: list) -> None:
-    sizes = []
-    renames = []
-    if not filenames:
-        return
-    for filename in filenames:
-        sizes.append(os.path.getsize(filename))
-    namemax = len("%u" % max(sizes))
-    for i, filename in enumerate(filenames):
-        dirpath, basename = os.path.split(filename)
-        namefix = os.path.splitext(basename)[-1]
-        namebase = "%0{}u".format(namemax) % (sizes[i])
-        sizename = os.path.join(dirpath, namebase + namefix).replace('\\', '/')
-        idx = 1
-        while sizename in renames:
-            sizename = os.path.join(
-                dirpath, namebase + '-%u' % (idx) + namefix
-            ).replace('\\', '/')
-            idx += 1
-        renames.append(sizename)
-    for i, filename in enumerate(filenames):
-        if filename != renames[i] and not os.path.exists(renames[i]):
-            if opt_rename:
-                os.rename(filename, renames[i])
-            if opt_verbose:
-                dirpath, srcname = os.path.split(filename)
-                dirpath, dstname = os.path.split(renames[i])
-                if not dirpath:
-                    dirpath = '.'
-                print("{}/{{{} <= {}}}".format(dirpath, dstname, srcname))
-
-
-def exe_walk(dir: str, func):
-    for dirpath, dirnames, filenames in os.walk(dir):
-        files = []
+    def exec(self, filenames):
+        self.files = []
         for filename in filenames:
-            filename = os.path.join(dirpath, filename).replace('\\', '/')
-            files.append(filename)
-        if files:
-            func(files)
-        del files
-        for dirname in dirnames:
-            dirname = os.path.join(dirpath, dirname).replace('\\', '/')
-            exe_walk(dirname, func)
+            if not os.path.isfile(filename):
+                continue
+            filesize = os.path.getsize(filename)
+            filehash = hash_(filename)
+            self.files.append((filename, filesize, filehash))
+        if self.files:
+            self.files.sort(key=lambda x: x[1])
+            self.init().rename().remove().exit()
+        del self.files
+
+    def init(self):
+        n = len(self.files)
+        self.stats = [0] * n
+        for i, file in enumerate(self.files):
+            if not file[1]:
+                self.stats[i] = -1
+                continue
+            for j in range(i + 1, n):
+                if self.files[i][1] != self.files[j][1]:
+                    break
+                if self.files[i][2] == self.files[j][2]:
+                    ti = os.path.getmtime(self.files[i][0])
+                    tj = os.path.getmtime(self.files[j][0])
+                    if ti > tj:
+                        self.stats[i] = 1
+                    else:
+                        self.stats[j] = 1
+        return self
+
+    def rename(self):
+        namefmt = "%0{}x".format(len("%x" % self.files[-1][1]))
+        for i, file in enumerate(self.files):
+            if self.stats[i] == 0:
+                dirname, srcname = os.path.split(file[0])
+                srcname, suffix = os.path.splitext(srcname)
+                dstname = namefmt % (file[1]) + file[2][:8]
+                filename = os.path.join(dirname, dstname + suffix)
+                if file[0] != filename:
+                    if self.rename_:
+                        os.rename(file[0], filename)
+        return self
+
+    def remove(self):
+        for i, file in enumerate(self.files):
+            if self.stats[i] != 0:
+                if self.remove_:
+                    os.remove(file[0])
+                if self.verbose:
+                    print("删除", file[0], file[1])
+        return self
+
+    def exit(self):
+        del self.stats
 
 
 if __name__ == "__main__":
-    for dir in opt_dirs:
-        exe_walk(dir, exe_remove)
-        exe_walk(dir, exe_rename)
-    exe_remove(opt_files)
-    exe_rename(opt_files)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dirs", nargs='+', help="dirs")
+    parser.add_argument("-r", "--remove", dest="remove", action="store_true")
+    parser.add_argument("-m", "--rename", dest="rename", action="store_true")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        help="explain what is being done",
+    )
+    args = parser.parse_args()
+    main = rm(remove=args.remove, rename=args.rename, verbose=args.verbose)
+    for dir in args.dirs:
+        if os.path.isdir(dir):
+            main(dir)
+    main.exec(args.dirs)
